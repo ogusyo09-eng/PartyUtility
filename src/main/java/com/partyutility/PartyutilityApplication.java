29e11cc840d3b090ce5a7547edb1b0f142f3d40d
ogusyo09-eng <ogusyo09@gmail.com>
2025-12-26 19:21:02 +0900

    Update

---
+++ src/main/java/com/partyutility/PartyutilityApplication.java
@@ -0,0 +1,13 @@
+package com.partyutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyutilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyutilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partyutility/PartyutilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partyutility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyutilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
