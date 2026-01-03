9b4ce73ff8f206d7ec16d3dce01268f66c8ce54e
ogusyo09-eng <ogusyo09@gmail.com>
2026-01-03 18:45:03 +0900

    Create SeatController.java

---
+++ src/main/java/com/partyutility/controller/SeatController.java
@@ -0,0 +1,19 @@
+package com.partyutility.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/seats")
+public class SeatController {
+
+    @GetMapping("/random")
+    public List<Integer> random(@RequestParam int people) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= people; i++) {
+            list.add(i);
+        }
+        Collections.shuffle(list);
+        return list;
+    }
+}
